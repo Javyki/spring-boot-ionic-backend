45cf07248b999714a42e04a20127a70f25a997e7
Javyki <javyki@gmail.com>
2019-07-13 12:02:12 -0300

    Endpoint  /pedidos/{id} disponibilizado

---
+++ src/main/java/com/david/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.david.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.david.cursomc.domain.Pedido;
+import com.david.cursomc.repositories.PedidoRepository;
+import com.david.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+
+}
